b39d0dbc4b7a48db3dc849e46d678d21c2c61d61
Дмитрий Приискалов <flowhack@outlook.com>
2020-11-09 22:26:06 +0400

    Initial beauty guidance

--- script.js
+++ script.js
@@ -65,4 +65,42 @@ $(document).ready(function(){
         }
         return false;
     });
+    $("#ubuntu_block").click(function () {
+        if ($("#ubuntu").is(":hidden")) {
+            $("#ubuntu").show("slow");
+            $("#windows").hide("slow");
+        }
+        return false;
+    });
+    $("#windows_block").click(function () {
+        if ($("#windows").is(":hidden")) {
+            $("#windows").show("slow");
+            $("#ubuntu").hide("slow");
+        }
+        return false;
+    });
+    $("#general_other_block").click(function () {
+        if ($("#general_other_block_content").is(":hidden")) {
+            $("#general_other_block_content").show("slow");
+            $("#optimization_content").hide("slow");
+            $("#password_content").hide("slow");
+        }
+        return false;
+    });
+    $("#optimization_block").click(function () {
+        if ($("#optimization_content").is(":hidden")) {
+            $("#optimization_content").show("slow");
+            $("#password_content").hide("slow");
+            $("#general_other_block_content").hide("slow");
+        }
+        return false;
+    });
+    $("#password_block").click(function () {
+        if ($("#password_content").is(":hidden")) {
+            $("#password_content").show("slow");
+            $("#optimization_content").hide("slow");
+            $("#general_other_block_content").hide("slow");
+        }
+        return false;
+    });
 });
